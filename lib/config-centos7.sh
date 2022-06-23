4321e3538652ffd9d03dd7c691d73fb3d43835fc
DevOps <3mops2021@gmail.com>
2022-06-23 10:38:22 +0800

    update.

---
+++ lib/config-centos7.sh
@@ -0,0 +1,15 @@
+# Copyright (c) 2022 Operator
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+yum install -y epel-release
+yum install -y bash-completion bash-completion-extras
+timedatectl set-timezone Asia/Hong_Kong
+yum upgrade -y
+yum update -y
+
+yum install -y ncdu
+
+yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+yum install -y ripgrep
