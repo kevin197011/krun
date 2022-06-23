3db8895bd5764da44f64d215208abb9c70aaad2e
DevOps <3mops2021@gmail.com>
2022-06-23 18:14:58 +0800

    update.

--- git.rb
+++ git.rb
@@ -4,13 +4,6 @@
 # https://opensource.org/licenses/MIT
 
 # git push
-# system 'rake'
-
-# require 'tty-command'
-# require 'rake'
-
-# cmd = TTY::Command.new
 Dir.chdir('sh') do
-  # cmd.run(:rake)
   system 'rake'
 end
---
+++ lib/apnic.rb
@@ -0,0 +1,31 @@
+# Copyright (c) 2022 Operator
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+require 'net/http'
+require 'uri'
+
+count_sn = 'CN'
+cache_file = 'apnic.txt'
+
+unless File.exist?(cache_file) || File.size?(cache_file)
+  uri = URI.parse('https://ftp.apnic.net/stats/apnic/delegated-apnic-latest')
+  request = Net::HTTP::Get.new(uri)
+  req_options = { use_ssl: uri.scheme == 'https' }
+  response =
+    Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+  File.write('apnic.txt', response.body)
+end
+
+pattern = /apnic\|#{count_sn}\|ipv4\|(?<ip>\d+\.\d+\.\d+\.\d+)\|(?<hosts>\d+)\|\d+\|allocated/mi
+
+File.readlines(cache_file).select do |line|
+  next unless line.match(pattern)
+
+  val = line.match(pattern)
+  netmask = 32 - Math.log2(val[:hosts].to_i).to_i
+  puts "#{val[:ip]}/#{netmask}"
+end
