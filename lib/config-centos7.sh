dc175e155031894592d2d139353c7203cf9fdef2
DevOps <3mops2021@gmail.com>
2022-06-23 11:57:50 +0800

    update.

--- lib/config-centos7.sh
+++ lib/config-centos7.sh
@@ -13,3 +13,5 @@ yum install -y ncdu
 
 yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
 yum install -y ripgrep
+
+yum install lrzsz -y
