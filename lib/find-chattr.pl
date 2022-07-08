0c25717db494dbf896bbe472aaacb14fe461a393
DevOps <3mops2021@gmail.com>
2022-07-08 15:03:23 +0800

    update.

--- lib/find-chattr.pl
+++ lib/find-chattr.pl
@@ -3,7 +3,24 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-@files = glob('/www/wwwroot/**/*.user.ini');
+# @files = glob('/www/wwwroot/**/*.user.ini');
+
+# foreach( @files ) {
+#   print "chattr -i $_\n";
+#   system "chattr -i $_";
+# }
+
+
+use File::Find;
+
+my @files;
+my @filepath=qw(/www/wwwroot/);
+
+sub find_ini_file {
+  push @files, $File::Find::name if (-f $File::Find::name and /^\.user\.ini$/);
+}
+
+find(\&find_ini_file, @filepath);
 
 foreach( @files ) {
   print "chattr -i $_\n";
