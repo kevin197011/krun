4ea2d3d7f740c705c1a76b04b6e4308f46d503ab
DevOps <3mops2021@gmail.com>
2022-07-15 09:40:17 +0800

    update.

---
+++ lib/purge-log.pl
@@ -0,0 +1,18 @@
+# Copyright (c) 2022 Operator
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+use File::Find;
+
+my @files;
+my @filepath=qw(/www/);
+
+sub find_match_file {
+  push @files, $File::Find::name if (-f $File::Find::name and /.*\.log$/);
+}
+
+find \&find_match_file, @filepath;
+
+unlink @files;
+
