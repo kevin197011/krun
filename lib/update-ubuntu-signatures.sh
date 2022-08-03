d85d68233c3a212e476f21878d1b336dcf2cd5dd
Norton <kevin197011@outlook.com>
2022-08-04 03:28:16 +0800

    update.

--- lib/update-ubuntu-signatures.sh
+++ lib/update-ubuntu-signatures.sh
@@ -8,3 +8,4 @@
 
 # wget -qO - ${repositories_release_key} | apt-key --keyring /etc/apt/trusted.gpg add -
 wget -qO - http://download.opensuse.org/repositories/home:/Provessor/xUbuntu_20.04/Release.key | apt-key --keyring /etc/apt/trusted.gpg add -
+wget -qO - http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-completions/xUbuntu_19.10/Release.key | apt-key --keyring /etc/apt/trusted.gpg add -
