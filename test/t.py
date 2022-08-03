1c282f320a7c2467b21d7c2dfea07f0fbb9b477b
Norton <kevin197011@outlook.com>
2022-08-04 07:04:55 +0800

    update.

--- test/t.py
+++ test/t.py
@@ -3,14 +3,7 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+import os
 
-import re
 
-
-pattern = re.compile(r'>(\S+\.sh|py|rb|pl)</a>')
-
-with open('test/data.txt', 'r') as f:
-    # print(f.read())
-    vals = pattern.findall(f.read())
-
-    print('  - ' + '\n  - '.join(vals))
+os.system("ls /tmp")
