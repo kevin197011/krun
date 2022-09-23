81c026016e75b1ace8bb55dec596c2011d43facb
kevin <kevin197011.l@gmail.com>
2022-09-23 20:34:46 +0800

    update.

--- test/t.py
+++ test/t.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
 # Copyright (c) 2022 Operator
 #
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-import os
 
+i = 0 
+i += 1
+i += 1
 
-os.system("ls /tmp")
+print(i)
