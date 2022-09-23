6fd93d74e799e4e84e249ae0f671f6ef0a8e5c82
kevin <kevin197011.l@gmail.com>
2022-09-23 20:52:18 +0800

    update.

--- lib/install-aapanel.sh
+++ lib/install-aapanel.sh
@@ -6,7 +6,7 @@
 # /bin/bash -c "$(curl -fsSL http://www.aapanel.com/script/install_6.0_en.sh)"
 script_tmp='/tmp/install_6.0_en.sh'
 
-curl http://www.aapanel.com/script/install_6.0_en.sh -o ${script_tmp}
+curl -s http://www.aapanel.com/script/install_6.0_en.sh -o ${script_tmp}
 
 echo "[Info] 涉及到交互选择,手动执行下面命令安装:"
 echo "bash ${script_tmp} && rm -rf ${script_tmp}"
