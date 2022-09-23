bfc074ed381cc88b64d45a2732962981bc5fddea
kevin <kevin197011.l@gmail.com>
2022-09-23 21:14:37 +0800

    update.

---
+++ test/t.sh
@@ -0,0 +1,8 @@
+# Copyright (c) 2022 kk
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+curl -o /usr/bin/krun https://raw.githubusercontent.com/kevin197011/sh/main/krun && chmod +x /usr/bin/krun
+krun init
+krun 5
