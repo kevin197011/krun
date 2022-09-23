a10d24e20c3e6fc4ecb9f83d3bcd19669d20979a
kevin <kevin197011.l@gmail.com>
2022-09-23 20:04:16 +0800

    update.

---
+++ lib/delete-log-in-crontab.sh
@@ -0,0 +1,47 @@
+# Copyright (c) 2022 Operator
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+mkdir -p /opt/scripts
+
+scritp_name='delete-log.sh'
+
+tee /opt/scripts/${scritp_name} <<EOF
+#!/usr/bin/env sh
+# Copyright (c) 2022 Operator
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+# days
+days=7
+
+# log path
+dirs=(
+    "/data/logs"
+) 
+
+# delete old logs
+for d in ${dirs[@]}; do
+    find ${d} -name "*.log" -type f -mtime +${days} -delete
+done
+EOF
+
+tee /etc/cron.daily/${scritp_name} <<EOF
+#!/usr/bin/env sh
+# Copyright (c) 2022 Operator
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+bash /opt/scripts/${scritp_name}
+EOF
+
+chmod +x /opt/scripts/${scritp_name}
+chown +x /etc/cron.daily/${scritp_name}
+
+
+# delete
+# rm -rf /opt/scripts/${scritp_name}
+# rm -rf /etc/cron.daily/${scritp_name}
