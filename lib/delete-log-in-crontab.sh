76dcd31088d3c07fa5e64f7fab3a76feaf2f61ee
kevin <kevin197011.l@gmail.com>
2022-09-23 20:43:57 +0800

    update.

--- lib/delete-log-in-crontab.sh
+++ lib/delete-log-in-crontab.sh
@@ -7,7 +7,7 @@ mkdir -p /opt/scripts
 
 scritp_name='delete-log.sh'
 
-tee /opt/scripts/${scritp_name} <<EOF
+tee /opt/scripts/${scritp_name} > /dev/null <<EOF
 #!/usr/bin/env sh
 # Copyright (c) 2022 Operator
 #
@@ -28,7 +28,9 @@ for d in \${dirs[@]}; do
 done
 EOF
 
-tee /etc/cron.daily/${scritp_name} <<EOF
+echo "add /opt/scripts/${scritp_name} ok!"
+
+tee /etc/cron.daily/${scritp_name} > /dev/null <<EOF
 #!/usr/bin/env sh
 # Copyright (c) 2022 Operator
 #
@@ -38,9 +40,13 @@ tee /etc/cron.daily/${scritp_name} <<EOF
 bash /opt/scripts/${scritp_name}
 EOF
 
+echo "add /etc/cron.daily/${scritp_name} ok!"
+
 chmod +x /opt/scripts/${scritp_name}
 chmod +x /etc/cron.daily/${scritp_name}
 
+echo "chmod /opt/scripts/${scritp_name} ok!"
+echo "chmod /etc/cron.daily/${scritp_name} ok!"
 
 # delete
 # rm -rf /opt/scripts/${scritp_name}
