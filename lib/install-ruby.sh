8bb6060b0329fec9faf7b41af8e2259a85a91b22
kevin <kevin197011.l@gmail.com>
2022-10-05 17:25:23 +0800

    update.

--- lib/install-ruby.sh
+++ lib/install-ruby.sh
@@ -21,6 +21,6 @@ grep -q '. $HOME/.asdf/asdf.sh' ~/.bashrc || echo '. $HOME/.asdf/asdf.sh' >>~/.b
 grep -q '. $HOME/.asdf/completions/asdf.bash' ~/.bashrc || echo '. $HOME/.asdf/completions/asdf.bash' >>~/.bashrc
 . ~/.bashrc
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf install ruby 3.2.0
-asdf global ruby 3.2.0
+asdf install ruby 3.1.2
+asdf global ruby 3.1.2
 ruby -v
