99412256d20b33a0919006ca1de0e07593cde2c7
kevin <kevin197011.l@gmail.com>
2023-03-26 18:33:26 +0800

    update.

--- lib/config-ssh.sh
+++ lib/config-ssh.sh
@@ -2,3 +2,8 @@
 #
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
+
+
+perl -i -pe 's/(\s*)(#*)(\s*)PasswordAuthentication(.*)/PasswordAuthentication no/g' /etc/ssh/sshd_config
+perl -i -pe 's/(\s*)(#*)(\s*)PermitRootLogin(.*)/PermitRootLogin no/g' /etc/ssh/sshd_config
+systemctl restart sshd
