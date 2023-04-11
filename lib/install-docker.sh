4c13299b341477fc2705b58725545f40a5104e06
kevin <kevin197011.l@gmail.com>
2023-04-11 19:16:26 +0800

    update.

--- lib/install-docker.sh
+++ lib/install-docker.sh
@@ -17,7 +17,8 @@ yum install -y yum-utils epel-release && \
 yum-config-manager \
         --add-repo \
         'https://download.docker.com/linux/centos/docker-ce.repo' && \
-yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && \ 
+yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 systemctl start docker && \
 systemctl enable docker && \
 docker version && \
