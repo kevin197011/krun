5599cfb2473246e11ad682e7273df0440da65ebb
kevin <kevin197011.l@gmail.com>
2023-04-14 18:04:17 +0800

    update.

--- lib/install-docker.sh
+++ lib/install-docker.sh
@@ -28,7 +28,7 @@ fi
 if [ -n "$(command -v apt-get)" ]; then
     sudo apt-get -y remove docker docker-engine docker.io containerd runc
     sudo apt-get -y update
-    sudo apt-get install ca-certificates curl gnupg
+    sudo apt-get install -y ca-certificates curl gnupg
     sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -36,9 +36,9 @@ if [ -n "$(command -v apt-get)" ]; then
         "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
         "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    
+    sudo apt-get -y update
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
     systemctl start docker && \
     systemctl enable docker && \
     docker version && \
