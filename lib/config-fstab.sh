44f507e5130882f66d83d392de2b1b8b8c385fd9
kevin <kevin197011.l@gmail.com>
2023-05-03 20:35:41 +0800

    update.

--- lib/config-fstab.sh
+++ lib/config-fstab.sh
@@ -25,7 +25,7 @@ mkdir -p ${m_path}
 uuid=$(blkid | grep -w ${name} | grep -Ewo '[[:xdigit:]]{8}(-[[:xdigit:]]{4}){3}-[[:xdigit:]]{12}')
 
 grep -q ${uuid} /etc/fstab && echo 'fstab already config disk name, exit!' && exit 2
-
-echo "UUID=${uuid} ${m_path}                       xfs     defaults        0 0"
+echo ": =>"
+echo "  UUID=${uuid} ${m_path}                       xfs     defaults        0 0"
 
 mount /dev/${name} ${m_path} && echo "UUID=${uuid} ${m_path}                       xfs     defaults        0 0" >> /etc/fstab
