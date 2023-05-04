12c6591b66cf47766499819f0662920bd026f85a
kevin <kevin197011.l@gmail.com>
2023-05-04 13:18:32 +0800

    update.

--- lib/install-xtrabackup.sh
+++ lib/install-xtrabackup.sh
@@ -3,9 +3,11 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+# XtraBackup
 yum install perl-Digest-MD5 -y
 yum install https://repo.percona.com/yum/percona-release-latest.noarch.rpm -y
 percona-release enable-only tools release
-yum install -y https://www.percona.com/downloads/XtraBackup/Percona-XtraBackup-2.4.4/\
-              binary/redhat/7/x86_64/percona-xtrabackup-24-2.4.4-1.el7.x86_64.rpm
+yum install -y https://www.percona.com/downloads/XtraBackup/Percona-XtraBackup-2.4.4/binary/redhat/7/x86_64/percona-xtrabackup-24-2.4.4-1.el7.x86_64.rpm
 yum install lz4 -y
+
+mkdir -pv /backup
