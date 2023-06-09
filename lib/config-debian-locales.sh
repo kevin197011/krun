60ee55d562d9f4c158a67e76d25feb73eb1b402e
kevin <kevin197011.l@gmail.com>
2023-06-09 16:37:59 +0800

    update.

--- lib/config-debian-locales.sh
+++
@@ -1,7 +0,0 @@
-apt-get clean -y
-apt-get update -y
-apt-get install locales -y
-grep -q 'LC_ALL=en_US.UTF-8' /etc/environment || echo "LC_ALL=en_US.UTF-8" >> /etc/environment
-grep -q 'en_US.UTF-8 UTF-8' /etc/locale.gen || echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-locale-gen en_US.UTF-8
--- lib/config-ssh.sh
+++ lib/config-ssh.sh
@@ -1,9 +1,35 @@
-# Copyright (c) 2022 Operator
+#!/usr/bin/env bash
+
+# Copyright (c) 2022 kk
 #
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+set -o errexit
+set -o nounset
+set -o pipefail
 
-perl -i -pe 's/(\s*)(#*)(\s*)PasswordAuthentication(.*)/PasswordAuthentication no/g' /etc/ssh/sshd_config
-perl -i -pe 's/(\s*)(#*)(\s*)PermitRootLogin(.*)/PermitRootLogin no/g' /etc/ssh/sshd_config
-systemctl restart sshd
+# base code
+krun::base::config::ssh() {
+  grep -q 'Debian' /etc/issue && version='debian'
+  os_version="${version:-'centos'}"
+  eval "${FUNCNAME/base/${os_version}}"
+}
+
+# centos code
+krun::centos::config::ssh() {
+  perl -i -pe 's/(\s*)(#*)(\s*)PasswordAuthentication(.*)/PasswordAuthentication no/g' /etc/ssh/sshd_config
+  perl -i -pe 's/(\s*)(#*)(\s*)PermitRootLogin(.*)/PermitRootLogin no/g' /etc/ssh/sshd_config
+  systemctl restart sshd
+}
+
+# debian code
+krun::debian::config::ssh() {
+  perl -i -pe 's/(\s*)(#*)(\s*)PasswordAuthentication(.*)/PasswordAuthentication no/g' /etc/ssh/sshd_config
+  perl -i -pe 's/(\s*)(#*)(\s*)PermitRootLogin(.*)/PermitRootLogin no/g' /etc/ssh/sshd_config
+  systemctl restart sshd
+}
+
+# run main
+# exec krun::base::config::ssh "$@"
+krun::base::config::ssh "$@"
