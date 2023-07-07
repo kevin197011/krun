675491ead1b24d5ad46c5548a2125e7e5a21b47c
kevin <kevin197011.l@gmail.com>
2023-07-07 20:31:03 +0800

    update.

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,16 @@ mkdir -pv ${install_path}/{bin,config}
 curl -o ${install_path}/bin/krun https://raw.githubusercontent.com/kevin197011/krun/main/bin/krun
 chmod +x ${install_path}/bin/krun
 
+# mac
 if command -v brew >/dev/null 2>&1; then
-  grep -q "${install_path}/bin" ~/.zshrc || echo "export PATH='$PATH':${install_path}/bin" >>~/.zshrc
-  zsh
-  krun status
-  exit 0
+    grep -q "${install_path}/bin" ~/.zshrc || echo "export PATH=\$PATH:${install_path}/bin" >>~/.zshrc
+    zsh
+    krun status
+    exit 0
 fi
 
-grep -q "${install_path}/bin" ~/.bashrc || echo "export PATH='$PATH':${install_path}/bin" >>~/.bashrc
+# other
+grep -q "${install_path}/bin" ~/.bashrc || echo "export PATH=\$PATH:${install_path}/bin" >>~/.bashrc
 bash
 krun status
 exit 0
