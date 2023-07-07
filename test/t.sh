6078aff93fe7e781e7aa0abefbc669b180832d8a
kevin <kevin197011.l@gmail.com>
2023-07-07 19:35:18 +0800

    update.

--- test/t.sh
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023 kk
-#
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
-curl -o /usr/bin/krun https://raw.githubusercontent.com/kevin197011/sh/main/krun && chmod +x /usr/bin/krun
-krun init
-krun 5
---
+++ tests/t.sh
@@ -0,0 +1,23 @@
+# Copyright (c) 2023 kk
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+curl -v 'https://github.com/kevin197011/krun/tree-commit-info/main/lib' \
+  -H 'authority: github.com' \
+  -H 'accept: application/json' \
+  -H 'accept-language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,fil;q=0.6' \
+  -H 'content-type: application/json' \
+  -H 'cookie: _octo=GH1.1.1111902740.1659935217; _device_id=3ea1d8be574f78df89614e1e8fd1775f; user_session=AshdYxWDHWlHOUA6FkbpSs_VSAdqrZznI9TZ0YgU4aTlohlz; __Host-user_session_same_site=AshdYxWDHWlHOUA6FkbpSs_VSAdqrZznI9TZ0YgU4aTlohlz; logged_in=yes; dotcom_user=kevin197011; color_mode=%7B%22color_mode%22%3A%22auto%22%2C%22light_theme%22%3A%7B%22name%22%3A%22light%22%2C%22color_mode%22%3A%22light%22%7D%2C%22dark_theme%22%3A%7B%22name%22%3A%22light%22%2C%22color_mode%22%3A%22light%22%7D%7D; preferred_color_mode=light; tz=Asia%2FManila; has_recent_activity=1; _gh_sess=dRkuFq9iaZx%2FxLAelfbNQ4TXNCeAa6l34XK1kG08W5PqvVbIKI1rN8SSk%2BwdpDtCqqzkhbpZaP%2Br2sqozW5Pz6vf2X%2FnsPwUDEwsLIwWMhFfbDME3RxYI6%2Fwje5kRuMW7IhHg77W9gJKeHxEi6WDNVK0HyXFCueNUD2aOfomSbE0TWhNScKWt%2BdekljBWVBk63v5tUvbzISTDrWXyYHS5RFfkK%2B7wyEos7R8kY6ecQEGcHTzAne9%2F8RX6P6aBME%2BVeRnWdrHLAKtqkwFlljGfRvRIbhPRPm8JOYdWxCGcvmECyPZUgbuNUs%3D--CI%2BXFciez%2F8PR0PS--NGq%2BSZvtuvCxyj408rmr1g%3D%3D' \
+  -H 'github-verified-fetch: true' \
+  -H 'if-none-match: W/"0083a5cc9cba60c3b2aef7a03fd20507"' \
+  -H 'referer: https://github.com/kevin197011/krun/tree/main/lib' \
+  -H 'sec-ch-ua: "Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' \
+  -H 'x-requested-with: XMLHttpRequest' \
+  --compressed
