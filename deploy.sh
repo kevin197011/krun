d5f3f78f9014d07ca58b7a46ba2c064f29ed2000
kk <61411931+kevin197011@users.noreply.github.com>
2024-03-25 10:19:06 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,7 @@ command -v brew >/dev/null && {
 }
 
 # other
+grep -q 'ubuntu' /etc/os-release && apt install python
 grep -q "${install_path}/bin" ~/.bashrc || echo "export PATH=\$PATH:${install_path}/bin" >>~/.bashrc
 bash && krun status
 exit 0
