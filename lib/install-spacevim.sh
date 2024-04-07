8f38b1553a2c92e14e22708c2e7e99178043c951
Norton <kevin197011@outlook.com>
2024-04-07 22:26:41 +0800

    merge

---
+++ lib/install-spacevim.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Copyright (c) 2024 kk
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# vars
+
+# run code
+krun::install::spacevim::run() {
+    # default platform
+    platform='debian'
+    # command -v apt >/dev/null && platform='debian'
+    command -v yum >/dev/null && platform='centos'
+    command -v brew >/dev/null && platform='mac'
+    eval "${FUNCNAME/::run/::${platform}}"
+}
+
+# centos code
+krun::install::spacevim::centos() {
+    krun::install::spacevim::common
+}
+
+# debian code
+krun::install::spacevim::debian() {
+    krun::install::spacevim::common
+}
+
+# mac code
+krun::install::spacevim::mac() {
+    krun::install::spacevim::common
+}
+
+# common code
+krun::install::spacevim::common() {
+    # https://spacevim.org/quick-start-guide/
+    curl -sLf https://spacevim.org/install.sh | bash
+}
+
+# run main
+krun::install::spacevim::run "$@"
