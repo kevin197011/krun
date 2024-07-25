f2244e22eda73f8e1a6caa96cd638aacaeb011f2
kk <kevin197011@outlook.com>
2024-07-25 10:01:57 +0000

    Update 2024-07-25 10:01:57 +0000.

---
+++ lib/install-lsyncd.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Copyright (c) 2024 kk
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# vars
+
+# run code
+krun::install::lsyncd::run() {
+    # default platform
+    platform='debian'
+    # command -v apt >/dev/null && platform='debian'
+    command -v yum >/dev/null && platform='centos'
+    command -v brew >/dev/null && platform='mac'
+    eval "${FUNCNAME/::run/::${platform}}"
+}
+
+# centos code
+krun::install::lsyncd::centos() {
+    krun::install::lsyncd::common
+}
+
+# debian code
+krun::install::lsyncd::debian() {
+    krun::install::lsyncd::common
+}
+
+# mac code
+krun::install::lsyncd::mac() {
+    krun::install::lsyncd::common
+}
+
+# common code
+krun::install::lsyncd::common() {
+    echo "${FUNCNAME}"
+}
+
+# run main
+krun::install::lsyncd::run "$@"
