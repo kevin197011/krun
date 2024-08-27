a513c763b045058ea29de7daa950aea8c06de4a3
kk <kevin197011@outlook.com>
2024-08-27 07:40:36 +0000

    Update 2024-08-27 07:40:36 +0000.

--- lib/delete-video.sh
+++ lib/delete-video.sh
@@ -37,9 +37,22 @@ krun::delete::video::mac() {
 
 # common code
 krun::delete::video::common() {
-    local video_path="${video_path_custom:-/data/record}"
-    find "${video_path}" -mtime +30 -name "*.mp4" -exec rm -rf {} \;
-    echo "Delete 30 days ago video done."
+    mkdir -p /opt/scripts
+
+    tee /opt/scripts/delete-video.sh <<EOF
+#!/usr/bin/env bash
+
+video_path="\${video_path_custom:-/data/record}"
+find "\${video_path}" -mtime +30 -name "*.mp4" -exec rm -rf {} \;
+echo "Delete 30 days ago video done."
+EOF
+
+    chmod +x /opt/scripts/delete-video.sh
+
+    (
+        crontab -l 2>/dev/null
+        echo "0 3 * * * /opt/scripts/delete-video.sh"
+    ) | crontab -
 }
 
 # run main
