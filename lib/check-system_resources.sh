4832deeb2c16f9058c82ac53e588c0a5d1b5b04b
kk <kevin197011@outlook.com>
2024-10-10 02:19:06 +0000

    Update 2024-10-10 02:19:06 +0000.

---
+++ lib/check-system_resources.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+# Copyright (c) 2024 kk
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# curl exec:
+# curl -fsSL https://raw.githubusercontent.com/kevin197011/krun/main/lib/check-system_resources.sh | bash
+
+# vars
+
+# run code
+krun::check::system_resources::run() {
+    # default platform
+    platform='debian'
+    # command -v apt >/dev/null && platform='debian'
+    command -v yum >/dev/null && platform='centos'
+    command -v brew >/dev/null && platform='mac'
+    eval "${FUNCNAME/::run/::${platform}}"
+}
+
+# centos code
+krun::check::system_resources::centos() {
+    krun::check::system_resources::common
+}
+
+# debian code
+krun::check::system_resources::debian() {
+    echo "------ CPU 信息 ------"
+    lscpu | grep -E 'Model name|Socket|Core|Thread'
+
+    echo "------ 内存使用情况 ------"
+    free -h
+
+    echo "------ 磁盘使用情况 ------"
+    df -h
+
+    echo "------ 磁盘大小 ------"
+    lsblk
+
+}
+
+# mac code
+krun::check::system_resources::mac() {
+    krun::check::system_resources::common
+}
+
+# common code
+krun::check::system_resources::common() {
+    echo "${FUNCNAME}"
+}
+
+# run main
+krun::check::system_resources::run "$@"
