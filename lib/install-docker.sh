d357894e4a2fb1db1befce236ea3256feab9f211
Norton <kevin197011@outlook.com>
2025-03-09 17:24:03 +0800

    Update 2025-03-09 17:24:03 +0800.

--- lib/install-docker.sh
+++ lib/install-docker.sh
@@ -31,12 +31,12 @@ krun::install::docker::debian() {
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
-    
+
     # Add the repository to Apt sources:
     echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
     sudo apt-get update
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
@@ -68,6 +68,8 @@ krun::install::docker::centos() {
 
 # mac code
 krun::install::docker::mac() {
+    brew uninstall --cask docker --force
+    brew uninstall --formula docker --force
     brew install docker
     krun::install::docker::common
 }
