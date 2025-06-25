fbee3fa031ec4f86157fec6bff1839619f9544c8
Norton <kevin197011@outlook.com>
2025-06-25 19:21:43 +0800

    Update 2025-06-25 19:21:43 +0800.

--- lib/mount-disk-data.sh
+++ lib/mount-disk-data.sh
@@ -17,6 +17,7 @@ readonly RED='\033[0;31m'
 readonly GREEN='\033[0;32m'
 readonly YELLOW='\033[1;33m'
 readonly BLUE='\033[0;34m'
+readonly CYAN='\033[0;36m'
 readonly NC='\033[0m' # No Color
 
 readonly DISK_DEVICE="/dev/sdb"
